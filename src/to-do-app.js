c2bf47f356ed1b428431ce162fb859cad2fc03c7
Alberto Fernández Gómez <afernandezgom@gmail.com>
2019-09-04 16:42:02 +0200

    Refactored to use lit-html and LitElement

--- src/to-do-app.js
+++ src/to-do-app.js
@@ -1,92 +1,91 @@
+import { LitElement, html, css } from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
 import "./to-do-item.js";
 
-const template = document.createElement("template");
-template.innerHTML = `
-<style>
-    :host {
-    display: block;
-    font-family: sans-serif;
-    text-align: center;
-    }
-
-    button {
-    border: none;
-    cursor: pointer;
-    }
-
-    ul {
-    list-style: none;
-    padding: 0;
-    }
-</style>
-<h1>To do</h1>
-
-<input type="text" placeholder="Add a new to do"></input>
-<button>✅</button>
-
-<ul id="todos"></ul>
-`;
-
-class TodoApp extends HTMLElement {
-  constructor() {
-    super();
-    this._shadowRoot = this.attachShadow({ mode: "open" });
-    this._shadowRoot.appendChild(template.content.cloneNode(true));
-
-    this.$todoList = this._shadowRoot.querySelector("ul");
-    this.$input = this._shadowRoot.querySelector("input");
-
-    this.$submitButton = this._shadowRoot.querySelector("button");
-    this.$submitButton.addEventListener("click", this._addTodo.bind(this));
+class TodoApp extends LitElement {
+  static get properties() {
+    return {
+      todos: { type: Array }
+    };
   }
 
-  _addTodo() {
+  constructor() {
+    super();
+    this.todos = [
+      { text: "Make a demo", checked: true },
+      { text: "The boring stuff", checked: false },
+      { text: "Setting properties", checked: false },
+      { text: "Setting attributes", checked: false },
+      { text: "Reflecting properties to attributes", checked: false },
+      { text: "Events", checked: false },
+      { text: "Wrap it up", checked: false }
+    ];
+  }
+
+  firstUpdated() {
+    this.$input = this.shadowRoot.querySelector("input");
+  }
+
+  _removeTodo(e) {
+    this.todos = this.todos.filter((todo, index) => {
+      return index !== e.detail;
+    });
+  }
+
+  _toggleTodo(e) {
+    this.todos = this.todos.map((todo, index) => {
+      return index === e.detail ? { ...todo, checked: !todo.checked } : todo;
+    });
+  }
+
+  _addTodo(e) {
+    e.preventDefault();
     if (this.$input.value.length > 0) {
-      this._todos.push({ text: this.$input.value, checked: false });
-      this._renderTodoList();
+      this.todos = [...this.todos, { text: this.$input.value, checked: false }];
       this.$input.value = "";
     }
   }
 
-  _toggleTodo(e) {
-    const todo = this._todos[e.detail];
-    this._todos[e.detail] = Object.assign({}, todo, {
-      checked: !todo.checked
-    });
-    this._renderTodoList();
-  }
-
-  _renderTodoList() {
-    this.$todoList.innerHTML = "";
-
-    this._todos.forEach((todo, index) => {
-      let $todoItem = document.createElement("to-do-item");
-      $todoItem.setAttribute("text", todo.text);
-
-      if (todo.checked) {
-        $todoItem.setAttribute("checked", "");
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        font-family: sans-serif;
+        text-align: center;
       }
-
-      $todoItem.setAttribute("index", index);
-      $todoItem.addEventListener("onRemove", this._removeTodo.bind(this));
-      $todoItem.addEventListener("onToggle", this._toggleTodo.bind(this));
-
-      this.$todoList.appendChild($todoItem);
-    });
+      button {
+        border: none;
+        cursor: pointer;
+        background-color: Transparent;
+      }
+      ul {
+        list-style: none;
+        padding: 0;
+      }
+    `;
   }
 
-  _removeTodo(e) {
-    this._todos.splice(e.detail, 1);
-    this._renderTodoList();
-  }
-
-  set todos(value) {
-    this._todos = value;
-    this._renderTodoList();
-  }
-
-  get todos() {
-    return this._todos;
+  render() {
+    return html`
+      <h1>To do</h1>
+      <form id="todo-input">
+          <input type="text" placeholder="Add a new to do"></input>
+          <button @click=${this._addTodo}>✅</button>
+      </form>
+      <ul id="todos">
+          ${this.todos.map(
+            (todo, index) => html`
+              <to-do-item
+                ?checked=${todo.checked}
+                .index=${index}
+                text=${todo.text}
+                @onRemove=${this._removeTodo}
+                @onToggle=${this._toggleTodo}
+              >
+              </to-do-item>
+            `
+          )}
+      </ul>
+    `;
   }
 }
 
